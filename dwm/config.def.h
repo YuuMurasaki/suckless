790574c0432113dc50821aeda15175fadef4e4f5
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-12-25 23:56:44 +0700

    format code && add browser shortcut

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -27,10 +27,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "KeePassXC",NULL,       NULL,       1 << 7,       0,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",   NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "KeePassXC", NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
@@ -59,13 +59,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *termcmd[]     = { "st", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -88,7 +90,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-   	{ MODKEY,                       XK_x,      spawn,          SHCMD("power_options") },
+	{ MODKEY,                       XK_x,      spawn,          SHCMD("power_options") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
