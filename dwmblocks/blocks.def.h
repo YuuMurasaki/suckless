27b510c89740e650ec9ec6621c2884a3fd2a3613
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-12-26 21:51:52 +0700

    add status bar

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,12 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+/* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+static const Block blocks[] = {
+    /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
+    { ":",         "sb_memory",            30,		                0 },
+    { "",           "sb_internet",          120,                    0 }
+    { ":",         "date +'%H:%M'",        60,                      0 },
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+/* Sets delimiter between status commands. NULL character ('\0') means no delimiter. */
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
