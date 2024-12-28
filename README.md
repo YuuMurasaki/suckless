d1bde014287dc4f761cb5f8a18657f8d0b83ad86
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-12-28 16:52:49 +0700

    add color description

---
+++ README.md
@@ -0,0 +1,12 @@
+### Suckless build
+- this is my build of suckless programs
+- There are dwm, dmenu and st
+
+## Patches
+# Dwm
++ alwayscenter
++ useless gap
+
+
+# St
++ 
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -9,11 +9,11 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 
 /* theme */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#89dceb";
+static const char col_gray1[]       = "#222222"; /* background color */
+static const char col_gray2[]       = "#444444"; /* inactive window border color */
+static const char col_gray3[]       = "#bbbbbb"; /* font color */
+static const char col_gray4[]       = "#eeeeee"; /* current tag and current window font color */
+static const char col_cyan[]        = "#89dceb"; /* top bar second color (blue) and active window border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
