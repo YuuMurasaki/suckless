9324a9f852d1399916f6ea974a483bdbff14b58c
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-01-13 22:20:55 +0700

    patch dwm

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 ### Patches
 # dwm
-+ alwayscenter
-+ useless gap
+    + dwm-alwayscenter-20200625-f04cac6.diff
+    + dwm-uselessgap-20211119-58414bee958f2.diff
+    + dwm-hide_vacant_tags-6.4.diff
 
 # st
-+ st-scrollback-ringbuffer-0.9.2.diff
-+ st-scrollback-float-0.9.2.diff
-
+    + st-scrollback-ringbuffer-0.9.2.diff
+    + st-scrollback-float-0.9.2.diff
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -9,9 +9,9 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Nerd Font:style:medium:size=14", "JetBrainsMono Nerd Font Mono:style:medium:size=20" };
 
 /* theme */
-static const char col_gray1[]       = "#222222"; /* background color */
+static const char col_gray1[]       = "#1e1e2e"; /* background color */
 static const char col_gray2[]       = "#444444"; /* inactive window border color */
-static const char col_gray3[]       = "#bbbbbb"; /* font color */
+static const char col_gray3[]       = "#cdd6f4"; /* font color */
 static const char col_gray4[]       = "#eeeeee"; /* current tag and current window font color */
 static const char col_cyan[]        = "#89dceb"; /* top bar second color (blue) and active window border color */
 static const char *colors[][3]      = {
@@ -21,19 +21,16 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
-	{ "FreeTube",   NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "librewolf",  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "Firefox",    NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "KeePassXC",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,49 +59,40 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]        = { "st", NULL };
-static const char *browsercmd[]     = { "librewolf", NULL };
-
-#include <X11/XF86keysym.h>
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        			function        argument */
-	{ MODKEY,                       XK_p,      			spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, 			spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      			spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_b,      			togglebar,      {0} },
-	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      			setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      			setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, 			zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    			view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      			killclient,     {0} },
-	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  			setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  			togglefloating, {0} },
-	{ MODKEY,                       XK_0,      			view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      			tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  			focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, 			focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  			tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, 			tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_q,      			quit,           {0} },
-	{ ControlMask|ShiftMask,        XK_Escape, 			spawn,          SHCMD("st -e htop 2>/dev/null") },
-	{ MODKEY,                       XK_e,      			spawn,          SHCMD("st -e lf 2>/dev/null") },
-	{ MODKEY,                       XK_x,      			spawn,          SHCMD("power_options") },
-	{ MODKEY|ShiftMask,             XK_s,      			spawn,          SHCMD("screenshot crop") },
-	{ 0,				XK_Print,  			spawn,          SHCMD("screenshot full") },
-
-	{ 0, 				XF86XK_AudioMute, 		spawn,         	SHCMD("volume mute") },
-	{ 0, 				XF86XK_AudioRaiseVolume,        spawn,          SHCMD("volume up") },
-	{ 0, 				XF86XK_AudioLowerVolume,        spawn,          SHCMD("volume down") },
-
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ ControlMask|ShiftMask,        XK_Escape, spawn,          SHCMD("st -e htop 2>/dev/null") },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("st -e lf 2>/dev/null") },
+	{ MODKEY,                       XK_x,      spawn,          SHCMD("power_options") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("screenshot crop") },
+	{ 0,				XK_Print,  spawn,          SHCMD("screenshot full") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
