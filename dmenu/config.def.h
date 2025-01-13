f8f97e165e7eabfe77af98c6aeed1bf077087281
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-01-13 17:06:48 +0700

    hehe

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font:style:medium:size=15", "JetBrainsMono Nerd Font Mono:style:medium:size=20" 
+	"Iosevka Nerd Font:style:medium:size=14", "JetBrainsMono Nerd Font Mono:style:medium:size=20" 
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
