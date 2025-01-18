5e1b40ea60a97a062a3d91aa3a461e815f504f8a
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-01-18 22:24:12 +0700

    remove font awesome support

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -5,8 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Iosevka:style:medium:size=14",
-	"JetBrainsMono Nerd Font Mono:style:medium:size=20",
-	"Font Awesome 6 Free:style:regular:size=14"
+	"JetBrainsMono Nerd Font Mono:style:medium:size=20"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
