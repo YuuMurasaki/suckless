6e1c793904c5ace04780005cc3b7fd84ecbac8e2
YuuMurasaki <hoangthibaongoc411@gmail.com>
2025-07-21 22:48:27 +0700

    add keybinding for pashmenu

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -99,6 +99,7 @@ static const Key keys[] = {
 	{ ControlMask|ShiftMask,        XK_Escape, spawn,          {.v = taskmanager } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = filemanager } },
 	{ MODKEY,                       XK_x,      spawn,          SHCMD("power_options") },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("pashmenu") },
 	{ MODKEY,                       XK_Insert, spawn,          SHCMD("bookmarks") },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("screenshot crop") },
 	{ 0,				XK_Print,  spawn,          SHCMD("screenshot full") },
